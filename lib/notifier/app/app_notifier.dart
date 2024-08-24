4bf8555f40cd5cd533e34942d4aff0152a1d1803
chandara-dgc <L.Chandara@dgc.gov.kh>
2024-08-24 23:31:09 +0700

    build

--- lib/notifier/app/app_notifier.dart
+++ lib/notifier/app/app_notifier.dart
@@ -1,7 +1,6 @@
 import 'package:check_list_app/router/app_router.dart';
 import 'package:check_list_app/utils/app_print/app_print.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AppNotifire extends ChangeNotifier {
